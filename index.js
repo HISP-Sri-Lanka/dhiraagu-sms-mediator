7f230ad2495a53e2bba0f516d771f6afe2eb41ee
ajhmohammed <50343026+ajhmohammed@users.noreply.github.com>
2024-11-14 10:36:43 +0530

    Inital Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+// const soap = require('soap')
+const cron = require('node-cron')
+// const sendsms = require('./sendsms')
+// const logging = require('./logging')
+var fs = require('node:fs')
+const dotenv = require('dotenv')
+// const getFhirResource = require('./getFhirResource')
+// const smsTemplate = require('./smsTemplate')
+dotenv.config();
+
+app.use(express.json());
+
+app.get('/sms', (req, res) => {   
+
+    res.send('loaded referredClients');    
+    
+})
+
+
+
+app.listen(process.env.PORT, () => {
+    (process.env.NODE_ENV !== 'prod') ? console.log(`Listening on port ${process.env.PORT}`): ''
+})
